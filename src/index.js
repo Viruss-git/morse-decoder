6e7e728854d330cefcebc1dd288163d190929456
MARS <Sergei-6@bk.ru>
2022-07-15 15:00:53 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -37,18 +37,56 @@ const MORSE_TABLE = {
     '-----': '0',
 };
 
-/*
 function decode(expr) {
-    // write your solution here
+    let arrBinar = Object.values(expr); // 
+    let arrLetters = []; // Собираем 10 значени для букв в массив
+    let subArr = []; // Собираем вложенный массив из массивов со значениями букв (по 10 элементов в каждом)
+
+    for (let i = 0; i <= arrBinar.length; i++) {
+        if (i > 0 && i % 10 === 0) { // Если i больше нуля и кратно 10, добавляем массив с буквами в subArr
+            subArr.push(arrLetters);
+            arrLetters = []; // Обнуляем массив для новых 10 значений
+            arrLetters.push(arrBinar[i]); // Добавляем первое значение что бы не пропускать иттерацию
+        } else {
+            arrLetters.push(arrBinar[i]); // Собирем значения в массив по 10
+        }
+    }
+
+    let arrayLetters = [];
+    for (let i = 0; i < subArr.length; i++) {
+        let indexStart = subArr[i].indexOf('1'); // Получаем индекс начала буквы
+        arrayLetters[i] = subArr[i].slice(indexStart).join('');
+    }
+    let letters = arrayLetters.join(' ').split(' ');
+
+    morseArray = letters.map(function (value) {
+        let morseArray = '';
+        for (let i = 0; i < value.length; i += 2) {
+            let letter = value.slice(i, i + 2);
+            switch (letter) {
+                case '*': morseArray += ' '; break;
+                case '10': morseArray += '.'; break;
+                case '11': morseArray += '-'; break;
+            }
+        }
+        return morseArray;
+    });
+
+    let keyMorseTable = Object.keys(MORSE_TABLE);
+    result = '';
+    morseArray.forEach(function (item, i) {
+        keyMorseTable.includes(item) ? result += MORSE_TABLE[item] : result += ' ';
+    });
+
+    return result;
 }
 
 module.exports = {
     decode
-}
+};
 
-*/
-const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
 
+/*
 function morse(expr) {
     let arrBinar = Object.values(expr); // 
     let arrLetters = []; // Собираем 10 значени для букв в массив
@@ -71,8 +109,6 @@ function morse(expr) {
     }
     let letters = arrayLetters.join(' ').split(' ');
 
-    // console.log(letters);
-
     morseArray = letters.map(function (value) {
         let morseArray = '';
         for (let i = 0; i < value.length; i += 2) {
@@ -85,30 +121,16 @@ function morse(expr) {
         }
         return morseArray;
     });
-    console.log(morseArray);
 
-    /* for (let item in MORSE_TABLE) {
- 
-         if (morseArray.includes(item)) {
-             result += MORSE_TABLE[item];
-         }
-     }*/
+    let keyMorseTable = Object.keys(MORSE_TABLE);
     result = '';
-    for (let item in MORSE_TABLE) {
+    morseArray.forEach(function (item, i) {
+        keyMorseTable.includes(item) ? result += MORSE_TABLE[item] : result += ' ';
+    });
 
-        console.log(item);
-        console.log(morseArray);
-
-        if (morseArray.includes(item)) {
-            result += MORSE_TABLE[item];
-        }
-
-
-    }
-
-
-    // console.log(result);
-
+    return result;
 }
 
+
 morse(expr);
+*/
